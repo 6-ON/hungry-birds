e49b79222558c0f9075e30768c6887c8a6ef6c3a
6-ON <aminhatim6@gmail.com>
2023-09-21 20:55:58 +0100

    HB-7 create leaderboard

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 const pregame = document.querySelectorAll('*[pregame]')
 const inGame = document.querySelectorAll('*[ingame]')
-const startBtn = document.querySelector('#start-btn')
+const postGame = document.querySelectorAll('*[postgame]')
+const startBtn = document.querySelectorAll('*[start-btn]')
 const holes = document.querySelectorAll('.hole')
 const worm = document.querySelector('#worm')
+const userName = document.querySelector('#user-name')
+let leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || []
+const leaderboardList = document.querySelector('#leaderboard-list')
+
+function updateLeaderboard(user) {
+    leaderboard = leaderboard.filter((u) => u.name !== user.name)
+    leaderboard.push(user)
+    leaderboard.sort((a, b) => a.score - b.score)
+    localStorage.setItem('leaderboard', JSON.stringify(leaderboard))
+    leaderboardList.innerHTML = `<tr>
+                        <th>Player Name</th>
+                        <th>Best Time</th>
+                        </tr>`
+    leaderboard.forEach((user) => {
+        leaderboardList.innerHTML += `<tr>
+                        <td>${user.name}</td>
+                        <td>${(user.score / 1000).toFixed(2)}s</td>
+                        </tr>`
+    })
+}
 const game = {
     score: 0,
     interval: null,
+    started_at: null,
 }
-let score = 0
 let activeHoles = []
-
+const user = {
+    name: '',
+    score: null,
+}
 function startGame() {
-    score = 0
-    pregame.forEach((element) => {
+    console.log('Game Started!');
+    if (userName.value === '' && user.name === '') {
+        alert('Please enter a name')
+        return
+    }
+    if (user.name === '') {
+    user.name = userName.value
+    }
+
+    game.score = 0
+    game.started_at = Date.now()
+    const l = [...pregame, ...postGame]
+    l.forEach((element) => {
+        console.log(element);
         element.classList.add('hidden')
     })
+
     inGame.forEach((element) => {
         element.classList.remove('hidden')
     })
     game.interval = startSpawing()
-    setTimeout(() => {}, 1000 * 60)
 }
-startBtn.addEventListener('click', startGame)
 
 function spawnBaby() {
     let hole = Math.floor(Math.random() * holes.length)
@@ -38,7 +73,7 @@ function spawnBaby() {
     baby.addEventListener('click', () => {
         if (birdState === 'hungry') {
             baby.src = 'assets/mole-game/fed.png'
-            score += 10
+            game.score += 10
             updateScore()
             birdState = 'fed'
         }
@@ -63,8 +98,8 @@ function spawnBaby() {
     holes[hole].appendChild(baby)
 }
 function updateScore() {
-    worm.style.width = `${score}%`
-    if (score === 100) {
+    worm.style.width = `${game.score}%`
+    if (game.score === 100) {
         endGame()
     }
 }
@@ -74,19 +109,28 @@ function startSpawing() {
     }, 1000)
 }
 function init() {
-    inGame.forEach((element) => {
+    const l = [...inGame, ...postGame]
+    l.forEach((element) => {
         element.classList.add('hidden')
+    })
+    userName.value = 'Player' + Math.floor(Math.random() * 1000)
+    startBtn.forEach((element) => {
+        element.addEventListener('click', startGame)
     })
 }
 function endGame() {
     clearInterval(game.interval)
-    pregame.forEach((element) => {
+    console.log('Game Over!')
+    user.score = Date.now() - game.started_at
+    console.log(user)
+    updateLeaderboard(user)
+    postGame.forEach((element) => {
         element.classList.remove('hidden')
     })
     inGame.forEach((element) => {
         element.classList.add('hidden')
     })
-    score = 0
+    game.score = 0
     updateScore()
 }
-init()
+// init()
